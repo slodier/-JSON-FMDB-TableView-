563dc43fb339c680d47b326adb74309000b6fa50
slodier <312301990@qq.com>
2016-09-04 18:13:50 +0800

    代码

---
+++ Test/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Test
+//
+//  Created by CC on 16/9/4.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic, copy)NSString *titleStr;
+@property (nonatomic, copy)NSString *contentStr;
+
+@end
